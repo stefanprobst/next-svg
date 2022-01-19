9f27ff290a635e10ec9818fe372f00e0c5f393c5
Stefan Probst <stefanprobst@posteo.de>
2022-01-19 12:39:44 +0100

    docs: update readme and types

--- src/svg.d.ts
+++ src/svg.d.ts
@@ -1,4 +1,28 @@
+declare module '*.svg?inline' {
+  import type { ReactNode, SVGProps, VFC } from 'react'
+
+  const Image: VFC<SVGProps<SVGSVGElement> & { title?: ReactNode }>
+
+  export default Image
+}
+
+declare module '*.svg?inline-icon' {
+  import type { ReactNode, SVGProps, VFC } from 'react'
+
+  const Image: VFC<SVGProps<SVGSVGElement> & { title?: ReactNode }>
+
+  export default Image
+}
+
 declare module '*.svg?symbol' {
+  import type { ReactNode, SVGProps, VFC } from 'react'
+
+  const Image: VFC<SVGProps<SVGSVGElement> & { title?: ReactNode }>
+
+  export default Image
+}
+
+declare module '*.svg?symbol-icon' {
   import type { ReactNode, SVGProps, VFC } from 'react'
 
   const Image: VFC<SVGProps<SVGSVGElement> & { title?: ReactNode }>
