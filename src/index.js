123a355ccd4324ea729933406114e1ba41341707
Stefan Probst <stefanprobst@kolabnow.com>
2022-01-20 20:01:37 +0100

    fix: inline all styles by default

--- src/index.js
+++ src/index.js
@@ -25,6 +25,9 @@ export default function createPlugin(pluginOptions = {}) {
                 params: {
                   overrides: {
                     removeViewBox: false,
+                    inlineStyles: {
+                      onlyMatchedOnce: false,
+                    },
                   },
                 },
               },
